c02cad5abacaf2e5cdfc1b8a569eb84e1175ae3e
Michael van Tellingen <michaelvantellingen@gmail.com>
2023-07-18 08:01:27 +0200

    Remove unused file

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  testEnvironment: "node",
-  testMatch: ['<rootDir>/src/**/*.test.{ts,tsx,js}'],
-  moduleFileExtensions: ['ts', 'js'],
-  transform: {
-    "^.+\\.tsx?$": "jest-esbuild",
-  },
-};
